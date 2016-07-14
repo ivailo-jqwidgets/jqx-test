7c92baaa8701c0037c229a6bd05f46664c57874b
ivailo-jqwidgets <ivailo@jqwidgets.com>
2016-07-14 16:05:23 +0300

    Removing true-false test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,8 @@ describe("A suite", function() {
     $("body").append("<jqx-test color='red' text='123'>123</jqx-test>");
 
     it('Should exist', function() {
-        mduri = document.body.getElementsByTagName("jqx-test");
-        console.log($(mduri).text());
-        expect($(mduri).text()).toBe('123');
-    });
-
-    it('Should exist', function() {
-        expect(true).toBe(true);
+        test = document.body.getElementsByTagName("jqx-test");
+        console.log($(test).text());
+        expect($(test).text()).toBe('123');
     });
 });
